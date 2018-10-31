7b7341c646a25da1568fafc169f6887f682b57cf
Alexei Verbitki <mailverbitski@gmail.com>
2018-10-31 10:03:48 +0200

    ImportAuthorizationEntity started

--- src/main/java/com/bass/amed/entity/ImportAuthorizationEntity.java
+++ src/main/java/com/bass/amed/entity/ImportAuthorizationEntity.java
@@ -2,285 +2,199 @@ package com.bass.amed.entity;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
-@Entity
-@Table(name = "import_authorization", schema = "amed", catalog = "")
-public class ImportAuthorizationEntity
-{
-    private Integer   id;
-    private Integer   stuffType;
-    private Integer   importerId;
-    private Integer   sellerId;
-    private Integer   producerId;
-    private Integer   nr;
-    private Timestamp startDate;
-    private Timestamp endDate;
-    private Integer   customsTransactionTypeId;
-    private Integer   customCodeId;
-    private Integer   sampling;
-    private Integer   returnedQuantity;
-    private Integer   customsDeclarationsNr;
-    private Timestamp customsDeclarationDate;
+@Entity @Table(name = "import_authorization", schema = "amed", catalog = "") public class ImportAuthorizationEntity {
+	private Integer                                id;
+	private String                                 applicationRegistrationNumber;
+	private Timestamp                              applicationDate;
+	private Integer                                applicant;
+	private NmManufacturesEntity                   seller;
+	private String                                 basisForImport;
+	private Integer                                importerId;
+	private String                                 conditionsAndSpecification;
+	private Integer                                quantity;
+	private Double                                 price;
+	private NmCurrenciesEntity                     currency;
+	private Double                                 summ;
+	private NmManufacturesEntity                   producer;
+	private Timestamp                              customsDeclarationDate;
+	private Timestamp                              expirationDate;
+	private NmCustomsCodesEntity                   customsCode;
+	private Integer                                customsNumber;
+	private NmTypesOfCustomsTransactionsEntity     customsTransactionType;
+	private String                                 authorizationsNumber;
+	private Integer                                medType;
+	private List<ImportAuthorizationDetailsEntity> importAuthorizationDetailsEntityList;
 
-    @Id
-    @Column(name = "id")
-    public Integer getId()
-    {
-        return id;
-    }
 
-    public void setId(Integer id)
-    {
-        this.id = id;
-    }
+	@Id
+	@Column(name = "id")
+	public Integer getId() {
+		return id;
+	}
 
-    @Basic
-    @Column(name = "stuff_type")
-    public Integer getStuffType()
-    {
-        return stuffType;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setStuffType(Integer stuffType)
-    {
-        this.stuffType = stuffType;
-    }
+	public String getApplicationRegistrationNumber() {
+		return applicationRegistrationNumber;
+	}
 
-    @Basic
-    @Column(name = "importer_id")
-    public Integer getImporterId()
-    {
-        return importerId;
-    }
+	public void setApplicationRegistrationNumber(String applicationRegistrationNumber) {
+		this.applicationRegistrationNumber = applicationRegistrationNumber;
+	}
 
-    public void setImporterId(Integer importerId)
-    {
-        this.importerId = importerId;
-    }
+	public Timestamp getApplicationDate() {
+		return applicationDate;
+	}
 
-    @Basic
-    @Column(name = "seller_id")
-    public Integer getSellerId()
-    {
-        return sellerId;
-    }
+	public void setApplicationDate(Timestamp applicationDate) {
+		this.applicationDate = applicationDate;
+	}
 
-    public void setSellerId(Integer sellerId)
-    {
-        this.sellerId = sellerId;
-    }
+	public Integer getApplicant() {
+		return applicant;
+	}
 
-    @Basic
-    @Column(name = "producer_id")
-    public Integer getProducerId()
-    {
-        return producerId;
-    }
+	public void setApplicant(Integer applicant) {
+		this.applicant = applicant;
+	}
 
-    public void setProducerId(Integer producerId)
-    {
-        this.producerId = producerId;
-    }
+	public NmManufacturesEntity getSeller() {
+		return seller;
+	}
 
-    @Basic
-    @Column(name = "nr")
-    public Integer getNr()
-    {
-        return nr;
-    }
+	public void setSeller(NmManufacturesEntity seller) {
+		this.seller = seller;
+	}
 
-    public void setNr(Integer nr)
-    {
-        this.nr = nr;
-    }
+	public String getBasisForImport() {
+		return basisForImport;
+	}
 
-    @Basic
-    @Column(name = "start_date")
-    public Timestamp getStartDate()
-    {
-        return startDate;
-    }
+	public void setBasisForImport(String basisForImport) {
+		this.basisForImport = basisForImport;
+	}
 
-    public void setStartDate(Timestamp startDate)
-    {
-        this.startDate = startDate;
-    }
+	public Integer getImporterId() {
+		return importerId;
+	}
 
-    @Basic
-    @Column(name = "end_date")
-    public Timestamp getEndDate()
-    {
-        return endDate;
-    }
+	public void setImporterId(Integer importerId) {
+		this.importerId = importerId;
+	}
 
-    public void setEndDate(Timestamp endDate)
-    {
-        this.endDate = endDate;
-    }
+	public String getConditionsAndSpecification() {
+		return conditionsAndSpecification;
+	}
 
-    @Basic
-    @Column(name = "customs_transaction_type_id")
-    public Integer getCustomsTransactionTypeId()
-    {
-        return customsTransactionTypeId;
-    }
+	public void setConditionsAndSpecification(String conditionsAndSpecification) {
+		this.conditionsAndSpecification = conditionsAndSpecification;
+	}
 
-    public void setCustomsTransactionTypeId(Integer customsTransactionTypeId)
-    {
-        this.customsTransactionTypeId = customsTransactionTypeId;
-    }
+	public Integer getQuantity() {
+		return quantity;
+	}
 
-    @Basic
-    @Column(name = "custom_code_id")
-    public Integer getCustomCodeId()
-    {
-        return customCodeId;
-    }
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
 
-    public void setCustomCodeId(Integer customCodeId)
-    {
-        this.customCodeId = customCodeId;
-    }
+	public Double getPrice() {
+		return price;
+	}
 
-    @Basic
-    @Column(name = "sampling")
-    public Integer getSampling()
-    {
-        return sampling;
-    }
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-    public void setSampling(Integer sampling)
-    {
-        this.sampling = sampling;
-    }
+	public NmCurrenciesEntity getCurrency() {
+		return currency;
+	}
 
-    @Basic
-    @Column(name = "returned_quantity")
-    public Integer getReturnedQuantity()
-    {
-        return returnedQuantity;
-    }
+	public void setCurrency(NmCurrenciesEntity currency) {
+		this.currency = currency;
+	}
 
-    public void setReturnedQuantity(Integer returnedQuantity)
-    {
-        this.returnedQuantity = returnedQuantity;
-    }
+	public Double getSumm() {
+		return summ;
+	}
 
-    @Basic
-    @Column(name = "customs_declarations_nr")
-    public Integer getCustomsDeclarationsNr()
-    {
-        return customsDeclarationsNr;
-    }
+	public void setSumm(Double summ) {
+		this.summ = summ;
+	}
 
-    public void setCustomsDeclarationsNr(Integer customsDeclarationsNr)
-    {
-        this.customsDeclarationsNr = customsDeclarationsNr;
-    }
+	public NmManufacturesEntity getProducer() {
+		return producer;
+	}
 
-    @Basic
-    @Column(name = "customs_declaration_date")
-    public Timestamp getCustomsDeclarationDate()
-    {
-        return customsDeclarationDate;
-    }
+	public void setProducer(NmManufacturesEntity producer) {
+		this.producer = producer;
+	}
 
-    public void setCustomsDeclarationDate(Timestamp customsDeclarationDate)
-    {
-        this.customsDeclarationDate = customsDeclarationDate;
-    }
+	public Timestamp getCustomsDeclarationDate() {
+		return customsDeclarationDate;
+	}
 
-    @Override
-    public int hashCode()
-    {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (stuffType != null ? stuffType.hashCode() : 0);
-        result = 31 * result + (importerId != null ? importerId.hashCode() : 0);
-        result = 31 * result + (sellerId != null ? sellerId.hashCode() : 0);
-        result = 31 * result + (producerId != null ? producerId.hashCode() : 0);
-        result = 31 * result + (nr != null ? nr.hashCode() : 0);
-        result = 31 * result + (startDate != null ? startDate.hashCode() : 0);
-        result = 31 * result + (endDate != null ? endDate.hashCode() : 0);
-        result = 31 * result + (customsTransactionTypeId != null ? customsTransactionTypeId.hashCode() : 0);
-        result = 31 * result + (customCodeId != null ? customCodeId.hashCode() : 0);
-        result = 31 * result + (sampling != null ? sampling.hashCode() : 0);
-        result = 31 * result + (returnedQuantity != null ? returnedQuantity.hashCode() : 0);
-        result = 31 * result + (customsDeclarationsNr != null ? customsDeclarationsNr.hashCode() : 0);
-        result = 31 * result + (customsDeclarationDate != null ? customsDeclarationDate.hashCode() : 0);
-        return result;
-    }
+	public void setCustomsDeclarationDate(Timestamp customsDeclarationDate) {
+		this.customsDeclarationDate = customsDeclarationDate;
+	}
 
-    @Override
-    public boolean equals(Object o)
-    {
-        if (this == o)
-        {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass())
-        {
-            return false;
-        }
+	public Timestamp getExpirationDate() {
+		return expirationDate;
+	}
 
-        ImportAuthorizationEntity that = (ImportAuthorizationEntity) o;
+	public void setExpirationDate(Timestamp expirationDate) {
+		this.expirationDate = expirationDate;
+	}
 
-        if (id != null ? !id.equals(that.id) : that.id != null)
-        {
-            return false;
-        }
-        if (stuffType != null ? !stuffType.equals(that.stuffType) : that.stuffType != null)
-        {
-            return false;
-        }
-        if (importerId != null ? !importerId.equals(that.importerId) : that.importerId != null)
-        {
-            return false;
-        }
-        if (sellerId != null ? !sellerId.equals(that.sellerId) : that.sellerId != null)
-        {
-            return false;
-        }
-        if (producerId != null ? !producerId.equals(that.producerId) : that.producerId != null)
-        {
-            return false;
-        }
-        if (nr != null ? !nr.equals(that.nr) : that.nr != null)
-        {
-            return false;
-        }
-        if (startDate != null ? !startDate.equals(that.startDate) : that.startDate != null)
-        {
-            return false;
-        }
-        if (endDate != null ? !endDate.equals(that.endDate) : that.endDate != null)
-        {
-            return false;
-        }
-        if (customsTransactionTypeId != null ? !customsTransactionTypeId.equals(that.customsTransactionTypeId) : that.customsTransactionTypeId != null)
-        {
-            return false;
-        }
-        if (customCodeId != null ? !customCodeId.equals(that.customCodeId) : that.customCodeId != null)
-        {
-            return false;
-        }
-        if (sampling != null ? !sampling.equals(that.sampling) : that.sampling != null)
-        {
-            return false;
-        }
-        if (returnedQuantity != null ? !returnedQuantity.equals(that.returnedQuantity) : that.returnedQuantity != null)
-        {
-            return false;
-        }
-        if (customsDeclarationsNr != null ? !customsDeclarationsNr.equals(that.customsDeclarationsNr) : that.customsDeclarationsNr != null)
-        {
-            return false;
-        }
-        if (customsDeclarationDate != null ? !customsDeclarationDate.equals(that.customsDeclarationDate) : that.customsDeclarationDate != null)
-        {
-            return false;
-        }
+	public NmCustomsCodesEntity getCustomsCode() {
+		return customsCode;
+	}
 
-        return true;
-    }
+	public void setCustomsCode(NmCustomsCodesEntity customsCode) {
+		this.customsCode = customsCode;
+	}
+
+	public Integer getCustomsNumber() {
+		return customsNumber;
+	}
+
+	public void setCustomsNumber(Integer customsNumber) {
+		this.customsNumber = customsNumber;
+	}
+
+	public NmTypesOfCustomsTransactionsEntity getCustomsTransactionType() {
+		return customsTransactionType;
+	}
+
+	public void setCustomsTransactionType(NmTypesOfCustomsTransactionsEntity customsTransactionType) {
+		this.customsTransactionType = customsTransactionType;
+	}
+
+	public String getAuthorizationsNumber() {
+		return authorizationsNumber;
+	}
+
+	public void setAuthorizationsNumber(String authorizationsNumber) {
+		this.authorizationsNumber = authorizationsNumber;
+	}
+
+	public Integer getMedType() {
+		return medType;
+	}
+
+	public void setMedType(Integer medType) {
+		this.medType = medType;
+	}
+
+	public List<ImportAuthorizationDetailsEntity> getImportAuthorizationDetailsEntityList() {
+		return importAuthorizationDetailsEntityList;
+	}
+
+	public void setImportAuthorizationDetailsEntityList(List<ImportAuthorizationDetailsEntity> importAuthorizationDetailsEntityList) {
+		this.importAuthorizationDetailsEntityList = importAuthorizationDetailsEntityList;
+	}
 }
